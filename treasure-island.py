2869289ec91fab2364902eb5afa6d3709c2bfe5d
LeoHo <109815756+howailok1120@users.noreply.github.com>
2023-09-08 01:28:00 +0800

    Create treasure-island.py

---
+++ treasure-island.py
@@ -0,0 +1,44 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+action_1 = input ("You see 2 doors in front of you, type 'left' to go to the left one or type 'right' to the right one.\n").lower()
+if action_1 == "left":
+  action_2 = input("Swim or wait?\n").lower()
+  if action_2 == "wait":
+    action_3 = input("You see three doors infront of you with different colors, pick one, they are colored in 'red', 'yellow', and 'blue'.\n").lower()
+    if action_3 == "red":
+      print("you burn by fire, gameover.")
+    elif action_3 == "yellow":
+      print("You win.")
+    elif action_3 == "blue":
+      print("You sucks, gameover.")
+  else:
+      print("attack by a trout, gameover.")
+else:
+    print("You fall into a hole, the end")
