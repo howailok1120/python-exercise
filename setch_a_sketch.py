c9aa92763d29cdad93cc268624833bcc73dd12d5
LeoHo <109815756+howailok1120@users.noreply.github.com>
2023-09-08 01:32:50 +0800

    Create setch_a_sketch.py

---
+++ setch_a_sketch.py
@@ -0,0 +1,36 @@
+import turtle
+
+tim = turtle.Turtle()
+tim.speed("fastest")
+screen = turtle.Screen()
+
+
+def move_forward():
+    tim.forward(10)
+
+
+def move_backward():
+    tim.backward(10)
+
+
+def turn_left():
+    tim.left(10)
+
+
+def turn_right():
+    tim.right(10)
+
+
+def clear():
+    tim.setposition(0.00, 0.00)
+    tim.clear()
+
+
+screen.listen()
+screen.onkeypress(key="w", fun=move_forward)
+screen.onkeypress(key="s", fun=move_backward)
+screen.onkeypress(key="a", fun=turn_left)
+screen.onkeypress(key="d", fun=turn_right)
+screen.onkeypress(key="c", fun=clear)
+
+screen.exitonclick()
