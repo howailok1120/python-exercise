8b47149eb498a319061e856ebabcf9ba31a233d5
LeoHo <109815756+howailok1120@users.noreply.github.com>
2023-09-08 01:35:55 +0800

    Add files via upload

--- Pong_Game/main.py
+++ Pong_Game/main.py
@@ -1,1 +1,52 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
 
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("The Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddle(350, 0)
+l_paddle = Paddle(-350, 0)
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(key="Up", fun=r_paddle.go_up)
+screen.onkey(key="Down", fun=r_paddle.go_down)
+screen.onkey(key="w", fun=l_paddle.go_up)
+screen.onkey(key="s", fun=l_paddle.go_down)
+
+
+game_is_on = True
+
+
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect ball go out of r_bound
+    if ball.xcor() > 360:
+        ball.reset_position()
+        scoreboard.l_point()
+        time.sleep(0.1)
+
+    # Detect ball go out of l_bound
+    if ball.xcor() < -360:
+        ball.reset_position()
+        scoreboard.r_point()
+
+screen.exitonclick()
